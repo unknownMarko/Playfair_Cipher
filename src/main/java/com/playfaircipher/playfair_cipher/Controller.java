a3c13a66e140f1cba72bcc596608edf28ee523fa
unknownMarko <marko.adamek@gmail.com>
2024-10-10 21:12:34 +0200

    Renamed class in Controller

--- src/main/java/com/playfaircipher/playfair_cipher/Controller.java
+++ src/main/java/com/playfaircipher/playfair_cipher/Controller.java
@@ -3,7 +3,7 @@ package com.playfaircipher.playfair_cipher;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
