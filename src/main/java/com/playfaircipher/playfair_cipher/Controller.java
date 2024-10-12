83bd9d26ee35460542694e4e167cfaeac078231e
unknownMarko <marko.adamek@gmail.com>
2024-10-12 07:13:42 +0200

    Fixed UI references.

--- src/main/java/com/playfaircipher/playfair_cipher/Controller.java
+++ src/main/java/com/playfaircipher/playfair_cipher/Controller.java
@@ -1,14 +1,74 @@
 package com.playfaircipher.playfair_cipher;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btn_decrypt;
+
+    @FXML
+    private Button btn_encrypt;
+
+    @FXML
+    private Label error_msg_alphabet;
+
+    @FXML
+    private Label error_msg_input;
+
+    @FXML
+    private Label error_msg_key_a;
+
+    @FXML
+    private Label error_msg_key_b;
+
+    @FXML
+    private TextField field_alphabet;
+
+    @FXML
+    private TextField field_input;
+
+    @FXML
+    private TextField field_input1;
+
+    @FXML
+    private TextField field_key_a;
+
+    @FXML
+    private TextField field_output;
+
+    @FXML
+    private Text text_input12;
+
+    @FXML
+    void handleCheckTextFieldKeyA(KeyEvent event) {
+
     }
+
+    @FXML
+    void handleCheckTextInput(KeyEvent event) {
+
+    }
+
+    @FXML
+    void handleDecryptButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEncryptButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleUpdateFieldAlphabet(KeyEvent event) {
+
+    }
+
 }
--- src/main/java/com/playfaircipher/playfair_cipher/PlayfairCipher.java
+++ src/main/java/com/playfaircipher/playfair_cipher/PlayfairCipher.java
@@ -10,14 +10,23 @@ import java.io.IOException;
 public class PlayfairCipher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PlayfairCipher.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(PlayfairCipher.class.getResource("playfair_cipher_frontend.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 480);
+        stage.setTitle("Playfair Cipher");
+        stage.setResizable(false);
         stage.setScene(scene);
+//           File iconFile = new File("src/main/resources/icon/icon_200.png");
+//           if (iconFile.exists()) {
+//               stage.getIcons().add(new Image(iconFile.toURI().toString()));
+//           } else {
+//               System.err.println("Could not find icon!");
+//           }
+        stage.centerOnScreen();
         stage.show();
     }
 
     public static void main(String[] args) {
+        PlayfairCipherLogic.printArray();
         launch();
     }
 }
